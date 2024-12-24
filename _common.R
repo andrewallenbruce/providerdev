51688b292a0bada778ef930c34b99668b82e93dc
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-12-23 22:34:18 -0500

    clean up

--- _common.R
+++ _common.R
@@ -1,3 +1,14 @@
+knitr::opts_chunk$set(
+  dev        = "ragg_png",
+  dpi        = 320,
+  out.width  = "100%",
+  fig.width  = 8,
+  fig.asp    = 0.818,
+  fig.retina = 2,
+  fig.align  = "center",
+  fig.show   = "hold"
+)
+
 btn_link <- function(href, label) {
 
   htmltools::tags$a(
@@ -9,3 +20,24 @@ btn_link <- function(href, label) {
     label
   )
 }
+
+purse <- function(x,
+                  pre = "- ",
+                  wid = 0,
+                  sep = " ") {
+  terse::terse(
+    x = x,
+    prefix = pre,
+    width = wid,
+    config = list(
+      gsep = sep,
+      ansi = FALSE))
+}
+
+options(scipen = 999, digits = 3)
+
+library(tidyverse)
+library(provider)
+library(fuimus)
+library(httr2)
+library(arrow)
