9f1216a8c22e34022f11f43b3b4d6bd70a8c057f
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-12-25 04:20:56 -0500

    openpayments

--- _common.R
+++ _common.R
@@ -9,7 +9,7 @@ knitr::opts_chunk$set(
   fig.show   = "hold"
 )
 
-btn_link <- function(href, label) {
+btn_link <- \(href, label) {
 
   htmltools::tags$a(
     href = href,
@@ -21,10 +21,7 @@ btn_link <- function(href, label) {
   )
 }
 
-purse <- function(x,
-                  pre = "- ",
-                  wid = 0,
-                  sep = " ") {
+purse <- \(x, pre = "- ", wid = 0, sep = " ") {
   terse::terse(
     x = x,
     prefix = pre,
@@ -32,6 +29,18 @@ purse <- function(x,
     config = list(
       gsep = sep,
       ansi = FALSE))
+}
+
+replace_open_columns <- \(x) stringr::str_replace_all(x, c(":" = "_", "%" = "", "@" = ""))
+remove_at_symbol     <- \(x) fuimus::sf_remove(s = x, p = "@", fix = TRUE)
+flatten_column       <- \(column) purrr::map_chr(column, \(x) paste0(fuimus::delist(x), collapse = ", "))
+
+clean_open_description <- \(x) {
+  stringr::str_replace_all(
+    x,
+    c("\n" = ". ",
+      "<p><strong>NOTE: </strong>This is a very large file and, depending on your network characteristics and software, may take a long time to download or fail to download. Additionally, the number of rows in the file may be larger than the maximum rows your version of <a href=\"https://support.microsoft.com/en-us/office/excel-specifications-and-limits-1672b34d-7043-467e-8e27-269d656771c3\">Microsoft Excel</a> supports. If you can't download the file, we recommend engaging your IT support staff. If you are able to download the file but are unable to open it in MS Excel or get a message that the data has been truncated, we recommend trying alternative programs such as MS Access, Universal Viewer, Editpad or any other software your organization has available for large datasets.</p>" = ""
+    ))
 }
 
 options(scipen = 999, digits = 3)
