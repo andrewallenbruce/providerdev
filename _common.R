ebea18ff13cb90eb49880d12ced0e678677408e1
Andrew Bruce <andrewbruce.himni@gmail.com>
2024-12-31 03:51:29 -0500

    PDC, clean up

--- _common.R
+++ _common.R
@@ -9,6 +9,8 @@ knitr::opts_chunk$set(
   fig.show   = "hold"
 )
 
+options(scipen = 999, digits = 3)
+
 btn_link <- \(href, label) {
 
   htmltools::tags$a(
@@ -21,23 +23,15 @@ btn_link <- \(href, label) {
   )
 }
 
-purse <- \(x, pre = "- ", wid = 0, max = 20, sep = " ") {
-  terse::terse(x = x, prefix = pre, width = wid, max_vec_len = max, config = list(gsep = sep, ansi = FALSE))
-}
+offset_sequence       <- \(rows, size = 5000) if (rows <= size) return(rows) else 0:round(rows / size) * size
+replace_fix           <- \(x, p, r, v = FALSE) stringi::stri_replace_all_fixed(str = x, pattern = p, replacement = r, vectorize_all = v)
+replace_open_columns  <- \(x) replace_fix(x, c(":", "%", "@", "$", "properties_"), c("_", "", "", "", "pr_"))
+replace_open_desc     <- \(x) replace_fix(x, c("\n", "<p><strong>NOTE: </strong>This is a very large file and, depending on your network characteristics and software, may take a long time to download or fail to download. Additionally, the number of rows in the file may be larger than the maximum rows your version of <a href=\"https://support.microsoft.com/en-us/office/excel-specifications-and-limits-1672b34d-7043-467e-8e27-269d656771c3\">Microsoft Excel</a> supports. If you can't download the file, we recommend engaging your IT support staff. If you are able to download the file but are unable to open it in MS Excel or get a message that the data has been truncated, we recommend trying alternative programs such as MS Access, Universal Viewer, Editpad or any other software your organization has available for large datasets.</p>"), c(". ", ""))
+remove_at_symbol      <- \(x) fuimus::sf_remove(s = x, p = "@", fix = TRUE)
+flatten_column        <- \(i) purrr::map_chr(i, \(x) paste0(fuimus::delist(x), collapse = ", "))
+purse                 <- \(x, pre = "- ", wid = 0, max = 20, sep = " ") terse::terse(x = x, prefix = pre, width = wid, max_vec_len = max, config = list(gsep = sep, ansi = FALSE))
 
-replace_open_columns <- \(x) stringr::str_replace_all(x, c(":" = "_", "%" = "", "@" = "", "$" = "", "properties_" = "pr_"))
-remove_at_symbol     <- \(x) fuimus::sf_remove(s = x, p = "@", fix = TRUE)
-flatten_column       <- \(i) purrr::map_chr(i, \(x) paste0(fuimus::delist(x), collapse = ", "))
-
-clean_open_description <- \(x) {
-  stringr::str_replace_all(
-    x,
-    c("\n" = ". ",
-      "<p><strong>NOTE: </strong>This is a very large file and, depending on your network characteristics and software, may take a long time to download or fail to download. Additionally, the number of rows in the file may be larger than the maximum rows your version of <a href=\"https://support.microsoft.com/en-us/office/excel-specifications-and-limits-1672b34d-7043-467e-8e27-269d656771c3\">Microsoft Excel</a> supports. If you can't download the file, we recommend engaging your IT support staff. If you are able to download the file but are unable to open it in MS Excel or get a message that the data has been truncated, we recommend trying alternative programs such as MS Access, Universal Viewer, Editpad or any other software your organization has available for large datasets.</p>" = ""
-    ))
-}
-
-recode_iso_8601 <- \(x) {
+recode_iso8601 <- \(x) {
   kit::nswitch(
     x,
     "R/P10Y",   "Decennially",
@@ -59,27 +53,10 @@ recode_iso_8601 <- \(x) {
     "R/P3M",    "Quarterly",
     "R/P4M",    "Three Times a Year",
     "R/P1W",    "Weekly",
-    "R/PT1H",   "Hourly")
-}
-
-main_catalog <- \() {
-
-  x <- arrow::read_json_arrow(
-    file = "https://data.cms.gov/data.json",
-    col_select = c("@context",
-                   "@id",
-                   "@type",
-                   "conformsTo",
-                   "describedBy"),
-    as_data_frame = FALSE) |>
-    dplyr::collect()
-
-  list(
-    context     = x$`@context`,
-    id          = x$`@id`,
-    type        = x$`@type`,
-    conformsTo  = x$conformsTo,
-    describedBy = x$describedBy)
+    "R/PT1H",   "Hourly",
+    default = NA_character_,
+    nThread = 4L
+    )
 }
 
 main_data_arrow <- \() {
@@ -153,10 +130,6 @@ provider_data <- \() {
     fuimus::remove_all_na()
 
 }
-
-offset_sequence <- \(rows, size = 5000) if (rows <= size) return(rows) else 0:round(rows / size) * size
-
-options(scipen = 999, digits = 3)
 
 library(collapse)
 library(tidyverse)
