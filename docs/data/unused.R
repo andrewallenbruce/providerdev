5dfbca1e4c95fc12a814580ad4c7c5ae4f708bdf
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-04-14 17:28:15 -0400

    Updated broken Main API code

--- docs/data/unused.R
+++ docs/data/unused.R
@@ -38,3 +38,85 @@ main_catalog <- \() {
     conformsTo  = x$conformsTo,
     describedBy = x$describedBy)
 }
+
+# btn_link <- \(href, label) {
+#
+#   htmltools::tags$a(
+#     href                    = href,
+#     class                   = "btn btn-outline-secondary",
+#     role                    = "button",
+#     target                  = "_blank",
+#     htmltools::tags$i(class = "bi bi-box-arrow-up-right"),
+#     label
+#   )
+# }
+
+
+# main_data_arrow <- \() {
+#
+#   x <- arrow::read_json_arrow(
+#     file          = "https://data.cms.gov/data.json",
+#     col_select    = c("dataset"),
+#     as_data_frame = TRUE) |>
+#     arrow::to_duckdb() |>
+#     dplyr::collect()
+#
+#   collapse::qTBL(x[["dataset"]][[1]]) |>
+#     collapse::fmutate(
+#       bureauCode   = delist(bureauCode),
+#       language     = delist(language),
+#       programCode  = delist(programCode),
+#       references   = delist(references),
+#       theme        = flatten_column(theme),
+#       keyword      = flatten_column(keyword)) |>
+#     collapse::frename(remove_at_symbol)
+# }
+
+# main_data_rcpp <- \() {
+#
+#   x <- RcppSimdJson::fload("https://data.cms.gov/data.json")
+#
+#   dataset <- collapse::qTBL(x[["dataset"]]) |>
+#     collapse::fmutate(
+#       bureauCode   = delist(bureauCode),
+#       language     = delist(language),
+#       programCode  = delist(programCode),
+#       references   = delist(references),
+#       theme        = flatten_column(theme),
+#       keyword      = flatten_column(keyword)) |>
+#     collapse::frename(remove_at_symbol)
+#
+#   distro <- collapse::fselect(dataset, distribution) |>
+#     tidyr::unnest(distribution) |>
+#     collapse::frename(remove_at_symbol)
+#
+#   list(
+#     context             = x[["@context"]],
+#     id                  = x[["@id"]],
+#     type                = x[["@type"]],
+#     conformsTo          = x[["conformsTo"]],
+#     describedBy         = x[["describedBy"]],
+#     dataset             = collapse::fselect(dataset, -distribution) |> remove_all_na(),
+#     distribution_latest = collapse::fsubset(distro, description %==% "latest") |> remove_all_na(),
+#     distribution_api    = collapse::fsubset(distro, not_na(format) & na(description)) |> remove_all_na(),
+#     distribution_csv    = collapse::fsubset(distro, mediaType %==% "text/csv") |> remove_all_na()
+#   )
+# }
+
+# provider_data <- \() {
+#   httr2::request("https://data.cms.gov/provider-data/api/1/metastore/schemas/dataset/items") |>
+#     httr2::req_perform() |>
+#     httr2::resp_body_json(simplifyVector = TRUE) |>
+#     dplyr::tibble() |>
+#     tidyr::unnest_wider(contactPoint, names_sep = "_") |>
+#     tidyr::unnest_wider(publisher, names_sep = "_") |>
+#     tidyr::unnest_wider(distribution, names_sep = "_") |>
+#     dplyr::mutate(
+#       bureauCode  = delist(bureauCode),
+#       programCode = delist(programCode),
+#       keyword     = flatten_column(keyword),
+#       theme       = flatten_column(theme)) |>
+#     dplyr::rename_with(remove_at_symbol) |>
+#     fuimus::remove_all_na() |>
+#     dplyr::filter(sf_ndetect(title, "Office Visit Costs$"))
+# }
