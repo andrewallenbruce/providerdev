f2d3cfba9fb29feb9f82a9b96a5827c305ae7b98
Andrew Bruce <andrewbruce.himni@gmail.com>
2025-05-09 18:31:18 -0400

    API sections

--- docs/includes/_common.R
+++ docs/includes/_common.R
@@ -1,3 +1,22 @@
+
+# https://blog.djnavarro.net/posts/2021-04-18_pretty-little-clis/
+
+ansi_aware_handler <- function(x, options) {
+  paste0(
+    "<pre class=\"r-output\"><code>",
+    fansi::sgr_to_html(x = x, warn = FALSE, term.cap = "256"),
+    "</code></pre>"
+  )
+}
+
+knitr::knit_hooks$set(
+  output = ansi_aware_handler,
+  message = ansi_aware_handler,
+  warning = ansi_aware_handler,
+  error = ansi_aware_handler
+)
+
+options(crayon.enabled = TRUE)
 options(scipen = 999, digits = 3)
 
 library(collapse)
@@ -38,42 +57,4 @@ purse <- \(
     max_vec_len = max,
     config = list(gsep = sep, ansi = FALSE)
   )
-}
-
-# @export
-# This will be exported by S7 next release.
-`:=` <- function(left, right) {
-  name <- substitute(left)
-  if (!is.symbol(name))
-    stop("left hand side must be a symbol")
-
-  right <- substitute(right)
-  if (!is.call(right))
-    stop("right hand side must be a call")
-
-  if (is.symbol(cl <- right[[1L]]) &&
-      as.character(cl) %in% c("function", "new.env")) {
-    # attach "name" attr for usage like:
-    # foo := function(){}
-    # foo := new.env()
-    right <- eval(right, parent.frame())
-    attr(right, "name") <- as.character(name)
-  } else {
-    # for all other usage,
-    # inject name as a named arg, so that
-    #   foo := new_class(...)
-    # becomes
-    #   foo <- new_class(..., name = "foo")
-
-    right <- as.call(c(as.list(right), list(name = as.character(name))))
-
-    ## skip check; if duplicate 'name' arg is an issue the call itself will signal an error.
-    # if (hasName(right, "name")) stop("duplicate `name` argument.")
-
-    ## alternative code path that injects `name` as positional arg instead
-    # right <- as.list(right)
-    # right <- as.call(c(right[[1L]], as.character(name), right[-1L]))
-  }
-
-  eval(call("<-", name, right), parent.frame())
 }
